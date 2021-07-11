b061d44483188347c8a1ada23b59d9814ee5b4bf
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-07-11 10:03:17 +0700

    Update shopping.ts

--- src/redux/reducers/shopping.ts
+++ src/redux/reducers/shopping.ts
@@ -1,28 +1,46 @@
 import { ShoppingAction } from '../actions'
-import { FoodAvailability, FoodModel, ShoppingState } from '../models'
+import { FoodAvailability, FoodModel, OfferModel, ShoppingState } from '../models'
 
 const initialState = {
     availability: {} as FoodAvailability,
-    availableFoods: {} as [FoodModel]
+    availableFoods: {} as [FoodModel],
+    offers: {} as [OfferModel],
+    appliedOffer: {} as OfferModel
 }
 
 export const shoppingReducer = (state: ShoppingState = initialState, action: ShoppingAction) => {
-    
-    switch(action.type){
+    switch (action.type) {
         case 'ON_AVAILABILITY':
             return {
                 ...state,
                 availability: action.payload
             }
+            
         case 'ON_FOODS_SEARCH':
             return {
                 ...state,
                 availableFoods: action.payload
             }
 
+        case 'ON_GET_OFFER':
+            return {
+                ...state,
+                offers: action.payload
+            }
+
+        case 'ON_APPLY_OFFER':
+            return {
+                ...state,
+                appliedOffer: action.payload
+            }
+        
+        case 'ON_REMOVE_OFFER':
+            return {
+                ...state,
+                appliedOffer: {}
+            }
+
         default:
             return state
     }
-
-
 }
