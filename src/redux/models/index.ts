63cc20fe651cd1e6b5357acbf5183dcd2d92b74f
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-07-11 10:03:13 +0700

    Update index.ts

--- src/redux/models/index.ts
+++ src/redux/models/index.ts
@@ -1,61 +1,94 @@
 import { Address } from 'expo-location'
 
-// Category
-export interface Category{
-    id: string,
-    title: String,
+export interface Category {
+    id: string
+    title: String
     icon: String
 }
 
-// Food Model
-export interface FoodModel{
-    _id: string;
-    name: string;
-    description: string;
-    category: string;
-    price: number;
-    readyTime: number;
-    images: [string];
-    unit: number;
+export interface FoodModel {
+    _id: string
+    name: string
+    description: string
+    category: string
+    price: number
+    readyTime: number
+    images: [string]
+    unit: number
 }
 
-// Restaurant Model
-export interface Restaurant{
-    _id: string;
-    name: string;
-    foodType: string;
-    address: string;
-    phone: string;
-    images: string;
-    foods: [FoodModel];
-}
-
-export interface FoodAvailability{
-    categories: [Category];
-    restaurants: [Restaurant];
+export interface Restaurant {
+    _id: string
+    name: string
+    foodType: string
+    address: string
+    phone: string
+    images: string
     foods: [FoodModel]
 }
 
-// todo : Modify later
-// User Model
-export interface UserModel{
-    firstName: string;
-    lastName: String;
-    contactNumber: String;
-    token: string,
-    varified: boolean
+export interface FoodAvailability {
+    categories: [Category]
+    restaurants: [Restaurant]
+    foods: [FoodModel]
 }
 
-export interface UserState{
-    user: UserModel;
-    location: Address;
-    error: string | undefined;
-    Cart: [FoodModel];
-    // orders
+export interface UserModel {
+    firstName: string
+    lastName: string
+    email: string
+    contactNumber: string
+    token: string
+    verified: boolean
 }
 
-export interface ShoppingState{
-    availability: FoodAvailability,
+export interface UserState {
+    user: UserModel
+    location: Address
+    error: string | undefined
+    Cart: [FoodModel]
+    orders: [OrderModel]
+}
+
+export interface ShoppingState {
+    availability: FoodAvailability
     availableFoods: [FoodModel]
-    // other models
+    offers: [OfferModel]
+    appliedOffer: OfferModel
+}
+
+export interface CartModel {
+    _id: string
+    food: FoodModel
+    unit: number
+}
+
+export interface OrderModel {
+    _id: string
+    orderID: string
+    items: [CartModel]
+    totalAmount: number
+    orderDate: number
+    paidThrough: string
+    paymentResource: string
+    orderStatus: string
+}
+
+export interface OfferModel {
+    _id: string
+    offerType: string
+    vendors: [any]
+    images: [string]
+    title: string
+    description: string
+    minValue: number
+    offerAmount: number
+    offerPercentage: number
+    startValidity: Date
+    endValidity: Date
+    promoCode: string
+    promoType: string
+    bank: [any]
+    bin: [any]
+    pincode: string
 }
