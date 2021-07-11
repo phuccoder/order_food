f1da5e9948de26a1e737ed9ce86b007f072a2740
Phuc Nguyen <40540902+phuccoder@users.noreply.github.com>
2021-07-11 10:02:57 +0700

    Update shopping.ts

--- src/redux/actions/shopping.ts
+++ src/redux/actions/shopping.ts
@@ -1,28 +1,40 @@
 import axios from 'axios'
 import { Dispatch } from 'react'
 import { BASE_URL } from '../../utils'
-import { FoodAvailability, FoodModel } from '../models'
-
-// availability Action
+import { FoodAvailability, FoodModel, OfferModel } from '../models'
 
 export interface AvailabilityAction {
-    readonly type: 'ON_AVAILABILITY',
+    readonly type: 'ON_AVAILABILITY'
     payload: FoodAvailability
 }
 
 export interface FoodSearchAction {
-    readonly type: 'ON_FOODS_SEARCH',
+    readonly type: 'ON_FOODS_SEARCH'
     payload: [FoodModel]
 }
 
 export interface ShoppingErrorAction {
-    readonly type: 'ON_SHOPPING_ERROR',
+    readonly type: 'ON_SHOPPING_ERROR'
     payload: any
 }
 
-export type ShoppingAction = AvailabilityAction | ShoppingErrorAction | FoodSearchAction 
+export interface OfferSearchAction {
+    readonly type: 'ON_GET_OFFER'
+    payload: [OfferModel]
+}
 
-// Trigger actions from components
+export interface ApplyOrRemoveOfferAction {
+    readonly type: 'ON_APPLY_OFFER' | 'ON_REMOVE_OFFER'
+    payload: OfferModel
+}
+
+export type ShoppingAction = 
+    AvailabilityAction | 
+    ShoppingErrorAction | 
+    FoodSearchAction |
+    OfferSearchAction |
+    ApplyOrRemoveOfferAction
+
 export const onAvailability = (postCode: string) => {
     return async ( dispatch: Dispatch<ShoppingAction>) => {
         try {
@@ -33,7 +45,6 @@ export const onAvailability = (postCode: string) => {
                     payload: 'Availability error'
                 })
             } else {
-                // save our location in local storage
                 dispatch({
                     type: 'ON_AVAILABILITY',
                     payload: response.data
@@ -48,7 +59,6 @@ export const onAvailability = (postCode: string) => {
     }
 }
 
-// Trigger actions from components
 export const onSearchFoods = (postCode: string) => {
     return async ( dispatch: Dispatch<ShoppingAction>) => {
         try {
@@ -56,10 +66,9 @@ export const onSearchFoods = (postCode: string) => {
             if (!response) {
                 dispatch({
                     type: 'ON_SHOPPING_ERROR',
-                    payload: 'Availability error'
+                    payload: 'Search error'
                 })
             } else {
-                // save our location in local storage
                 dispatch({
                     type: 'ON_FOODS_SEARCH',
                     payload: response.data
@@ -72,6 +81,49 @@ export const onSearchFoods = (postCode: string) => {
             })
         }
     }
+}
+
+export const onGetOffers = (postCode: string) => {
+    return async ( dispatch: Dispatch<ShoppingAction>) => {
+        try {
+            const response = await axios.get<[OfferModel]>(`${BASE_URL}food/offers/${postCode}`)
+            if (!response) {
+                dispatch({
+                    type: 'ON_SHOPPING_ERROR',
+                    payload: 'Get offer error'
+                })
+            } else {
+                dispatch({
+                    type: 'ON_GET_OFFER',
+                    payload: response.data
+                })
+            }
+        } catch (error) {
+            dispatch({
+                type: 'ON_SHOPPING_ERROR',
+                payload: error
+            })
+        }
+    }
+}
+
+export const onApplyOffer = (item: OfferModel, isRemove: boolean) => {
+    return async ( dispatch: Dispatch<ShoppingAction> ) => {
+        if (isRemove) {
+            dispatch({
+                type: 'ON_REMOVE_OFFER',
+                payload: item
+            })
+            console.log('REMOVE OFFER OK')
+        } else {
+            dispatch({
+                type: 'ON_APPLY_OFFER',
+                payload: item
+            })
+            console.log('APPLY OFFER OK')
+        }
+    }
+
 }
 
 
